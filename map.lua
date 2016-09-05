b356ebd6a6543262e1ffcec2b6f1aac245afe1f5
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-09-04 21:24:05 -0700

    init

---
+++ map.lua
@@ -0,0 +1,12 @@
+vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 48.2736053466797 }
+
+-- I round up the Z values to nearest hundredth
+spawnpoint 's_m_y_sheriff_01' { x = 1812.07, y = 3678.91, z = 34.277 }
+spawnpoint 's_m_y_sheriff_01' { x = 1806.78, y = 3676.12, z = 34.277 }
+spawnpoint 's_m_y_sheriff_01' { x = 1815.75, y = 3660.44, z = 34.212 }
+spawnpoint 's_m_y_sheriff_01' { x = 1830.42, y = 3667.61, z = 34.277 }
+spawnpoint 's_m_y_sheriff_01' { x = 1847.56, y = 3677.71, z = 34.278 }
+spawnpoint 's_m_y_sheriff_01' { x = 1865.58, y = 3688.88, z = 34.268 }
+spawnpoint 's_m_y_sheriff_01' { x = 1875.01, y = 3704.4, z = 34.27 }
+spawnpoint 's_m_y_sheriff_01' { x = 1840.46, y = 3694.22, z = 34.268 }
+spawnpoint 's_m_y_sheriff_01' { x = 1815.66, y = 3669.15, z = 34.278 }
